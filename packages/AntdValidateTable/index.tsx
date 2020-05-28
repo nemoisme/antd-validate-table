370cd101f025851d755bc0afc05e64326d47c7d2
nemoisme <apple@appledeMacBook-Pro.local>
2020-05-28 17:55:21 +0800

    fix:修复打包样式丢失的问题

--- packages/AntdValidateTable/index.tsx
+++ packages/AntdValidateTable/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useImperativeHandle, useCallback, forwardRef, useEffect } from 'react';
 import Table from 'antd/lib/table'
 import Form from 'antd/lib/form'
+import 'antd/dist/antd.css'
 import set from 'lodash.set'
 import { EditableColumn, EleParmas, optionItem, IProps, renderOps } from './type'
 
