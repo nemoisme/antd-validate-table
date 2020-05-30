892ba5f1ff1a3195ae54ea275ad05c347363180a
nemoisme <apple@appledeMacBook-Pro.local>
2020-05-30 16:50:29 +0800

    feat:调整目录文件

--- packages/AntdValidateTable/index.tsx
+++ packages/AntdValidateTable/index.tsx
@@ -5,15 +5,9 @@ import 'antd/es/table/style/index.css'
 import 'antd/es/form/style/index.css'
 import set from 'lodash.set'
 import { EditableColumn, EleParmas, optionItem, IProps, renderOps } from './type'
-
-
-const RENDER_MAP: object = {
-  Select: 'Option',
-  Radio: 'Group'
-}
+import {RENDER_MAP,formateInit,multilColumns} from './shared'
 
 const h = React.createElement
-
 
 const RenderEle = ({ val, rowIndex, col, record }: EleParmas): JSX.Element => {
   const { component, options, componentProps, componentChild } = typeof col.config == 'function' && col.config(rowIndex, record)
@@ -47,33 +41,12 @@ const renderCell = ({ val, record, rowIndex, col }: EleParmas): JSX.Element => {
 }
 
 
-const multilColumns = (columns: Array<any>): Array<any> => {
-  return columns.map((col) => ({
-    ...col,
-    render: (val, record, rowIndex) => renderCell({ val, record, rowIndex, col }),
-    children: col.children && multilColumns(col.children)
-  }))
-}
-
-
-const formateInit = (data: any[]) => {
-  return data.reduce((cur, next, index) => {
-    const rowItem = Object.keys(next).reduce((row, key) => {
-      row[`${index}.${key}`] = next[key]
-      return row
-    }, {})
-    cur = { ...cur, ...rowItem }
-    return cur
-  }, {})
-}
-
-
 const AntdValidateTable = (props: IProps, ref): JSX.Element => {
   const { dataSource, columns, formAttrs, tableAttrs } = props
   const [form] = Form.useForm()
   const [tableList, setTableList] = useState(dataSource)
   const [initForm, setInitForm] = useState(formateInit(dataSource))
-  const [tableColumns, setTableColumns] = useState(multilColumns(columns))
+  const [tableColumns, setTableColumns] = useState(multilColumns(columns,renderCell))
 
   useEffect(() => {
     setTableList(dataSource)
--- packages/AntdValidateTable/shared.ts
+++ packages/AntdValidateTable/shared.ts
@@ -1,1 +1,26 @@
+export const RENDER_MAP: object = {
+  Select: 'Option',
+  Radio: 'Group'
+}
+
+export const formateInit = (data: any[]) => {
+  return data.reduce((cur, next, index) => {
+    const rowItem = Object.keys(next).reduce((row, key) => {
+      row[`${index}.${key}`] = next[key]
+      return row
+    }, {})
+    cur = { ...cur, ...rowItem }
+    return cur
+  }, {})
+}
+
+
+export const multilColumns = (columns: Array<any>, renderFunc: any): Array<any> => {
+  return columns.map((col) => ({
+    ...col,
+    render: (val, record, rowIndex) => renderFunc({ val, record, rowIndex, col }),
+    children: col.children && multilColumns(col.children, renderFunc)
+  }))
+}
+
 
